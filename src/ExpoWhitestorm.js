9554706a9dbfebb062d54eb6edad4488d36d3ae9
Evan Bacon <baconbrix@gmail.com>
2017-11-14 14:08:45 -0800

    updated package

--- src/ExpoWhitestorm.js
+++
@@ -1,5 +0,0 @@
-export WhitestormView from './components/WhitestormView';
-export ElementModule from './modules/ElementModule';
-export EndFrameModule from './modules/EndFrameModule';
-export ResizeModule from './modules/ResizeModule';
-// export StatsModule from './modules/StatsModule';
